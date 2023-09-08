adc45246f9ec12b446553f7aca8c5315afb8dd77
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-09-08 18:12:32 -0300

    Adicionando estilos aos componentes plano antecipado e plano economico

--- src/app/components/main/plano-antecipado/plano-antecipado.component.ts
+++ src/app/components/main/plano-antecipado/plano-antecipado.component.ts
@@ -5,9 +5,17 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-plano-antecipado',
   templateUrl: './plano-antecipado.component.html',
+  styleUrls: ['./plano-antecipado.component.css'],
   standalone: true,
   imports: [MatExpansionModule, MatInputModule],
 })
 export class PlanoAntecipadoComponent {
+
+  planDescription: string = 'Neste plano, você recebe no próximo dia útil';
+  
+
+
+
+
   panelOpenState = false;
 }
--- src/app/components/main/plano-economico/plano-economico.component.ts
+++ src/app/components/main/plano-economico/plano-economico.component.ts
@@ -5,9 +5,13 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-plano-economico',
   templateUrl: './plano-economico.component.html',
+  styleUrls: ['./plano-economico.component.css'],
   standalone: true,
   imports: [MatExpansionModule, MatInputModule],
 })
 export class PlanoEconomicoComponent {
+
+  planDescription: string = 'Neste plano, você recebe mensalmente';
+
   panelOpenState = false;
 }
