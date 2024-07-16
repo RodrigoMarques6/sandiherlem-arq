0e779ca6e7985a0a8d66fdb9988d8b354ddce5d6
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-07-15 22:00:35 -0300

    Adicionando links e navegacao ao menu

--- src/app/components/main/economic-table/economic-table.component.ts
+++ src/app/components/main/economic-table/economic-table.component.ts
@@ -4,28 +4,119 @@ import { Router } from '@angular/router';
 import { SharedService } from 'src/app/services/shared.service';
 
 export interface PeriodicElement {
-  taxa: string,
-  modalidade: string,
-  desconto: number,
-  receba_total: number,
-  receba_mes: number,
-  cliente_paga_mes: number,
-};
+  taxa: string;
+  modalidade: string;
+  desconto: number;
+  receba_total: number;
+  receba_mes: number;
+  cliente_paga_mes: number;
+}
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {modalidade: 'Débito', taxa: 'Hydrogen', desconto: 1.0079, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '1x', taxa: 'Helium', desconto: 4.0026, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '2x', taxa: 'Lithium', desconto: 6.941, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '3x', taxa: 'Beryllium', desconto: 9.0122, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '4x', taxa: 'Boron', desconto: 10.811, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '5x', taxa: 'Carbon', desconto: 12.0107, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '6x', taxa: 'Nitrogen', desconto: 14.0067, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '7x', taxa: 'Oxygen', desconto: 15.9994, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '8x', taxa: 'Fluorine', desconto: 18.9984, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '9x', taxa: 'Neon', desconto: 20.1797, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '10x', taxa: 'Neon', desconto: 20.1797, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '11x', taxa: 'Neon', desconto: 20.1797, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
-  {modalidade: '12x', taxa: 'Neon', desconto: 20.1797, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {
+    modalidade: 'Débito',
+    taxa: 'Hydrogen',
+    desconto: 1.0079,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '1x',
+    taxa: 'Helium',
+    desconto: 4.0026,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '2x',
+    taxa: 'Lithium',
+    desconto: 6.941,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '3x',
+    taxa: 'Beryllium',
+    desconto: 9.0122,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '4x',
+    taxa: 'Boron',
+    desconto: 10.811,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '5x',
+    taxa: 'Carbon',
+    desconto: 12.0107,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '6x',
+    taxa: 'Nitrogen',
+    desconto: 14.0067,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '7x',
+    taxa: 'Oxygen',
+    desconto: 15.9994,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '8x',
+    taxa: 'Fluorine',
+    desconto: 18.9984,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '9x',
+    taxa: 'Neon',
+    desconto: 20.1797,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '10x',
+    taxa: 'Neon',
+    desconto: 20.1797,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '11x',
+    taxa: 'Neon',
+    desconto: 20.1797,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
+  {
+    modalidade: '12x',
+    taxa: 'Neon',
+    desconto: 20.1797,
+    receba_total: 1000,
+    receba_mes: 1000,
+    cliente_paga_mes: 1000,
+  },
 ];
 
 @Component({
@@ -36,8 +127,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
   imports: [MatTableModule],
 })
 export class EconomicTableComponent implements OnInit {
-
-  displayedColumns: string[] = ['modalidade', 'taxa', 'desconto', 'receba_total', 'receba_mes', 'cliente_paga_mes'];
+  displayedColumns: string[] = [
+    'modalidade',
+    'taxa',
+    'desconto',
+    'receba_total',
+    'receba_mes',
+    'cliente_paga_mes',
+  ];
   dataSource = ELEMENT_DATA;
 
   descriptionTable: string = `Valores válidos para um projeto de R$ `;
@@ -47,9 +144,8 @@ export class EconomicTableComponent implements OnInit {
     this.valueInput1 = this.sharedData.getNumber();
   }
 
-  constructor (private router: Router, private sharedData: SharedService) {}
+  constructor(private router: Router, private sharedData: SharedService) {}
   goToStart() {
-    this.router.navigate(['/inicio'])
-    }
-
+    this.router.navigate(['/inicio']);
+  }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,18 +3,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   standalone: true,
-  imports: [
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule
-  ]
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
 })
 export class MenuComponent {
-
+  constructor(private router: Router) {}
+  goToStart() {
+    this.router.navigate(['/inicio']);
+  }
 }
